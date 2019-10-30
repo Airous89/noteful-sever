a41940fae696d938bb3adf9dca8d4ffa42f5c352
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-10-30 19:49:07 -0400

    Add files via upload

--- src/folder-router.js
+++ src/folder-router.js
@@ -35,7 +35,7 @@ folderRouter
     .then(folder => {
       res
         .status(201)
-        .location(`http://localhost:8000/api/folder/${folder.id}`)
+        .location(`postgres://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q/api/folder/${folder.id}`)
         .json({ id: folder.id.toString(), name: xss(folder.folder_name) });
     })
     .catch(next);
--- src/note-router.js
+++ src/note-router.js
@@ -43,7 +43,7 @@ noteRouter
       .then(note => {
         res
           .status(201)
-          .location(`http://localhost:8000/note/${note.id}`)
+          .location(`postgres://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q/note/${note.id}`)
           .json({
             id: note.id.toString(),
             name: xss(note.note_name),
