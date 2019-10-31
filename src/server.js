60288a4d83fd1851598487253fe9ba18f51d47d4
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-10-30 20:22:34 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,5 +11,5 @@ const db = knex({
 
 app.set('db', db)
 app.listen(PORT, () => {
-  console.log(`Server listening at postgres://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q:${PORT}`)
+  console.log(`Server listening at https://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q:${PORT}`)
 })
